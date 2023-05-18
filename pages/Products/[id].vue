0579af0d867d416206418715c41d14654367d1bc
Luis <lsuizo72@gmail.com>
2023-05-19 01:57:48 +0800

    naaNayView

---
+++ pages/Products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Few details about products. and then a few and the a few and then a few expounding ideas.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
