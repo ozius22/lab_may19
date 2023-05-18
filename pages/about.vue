e8aca73313a49ff5e47f12434feb8a6111c75b18
Luis <lsuizo72@gmail.com>
2023-05-19 01:41:38 +0800

    pages

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Text goes here.</p>
+        <p>And, also here.</p>
+        <p>As well as here.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
